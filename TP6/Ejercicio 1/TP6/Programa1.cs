81c55e428d81e770bbbe916c25c122d1be7b9023
Chicabytes <chocobarjulietain@gmail.com>
2020-05-27 19:51:38 -0300

    Se agrega el ejercicio 1 y 2

---
+++ TP6/Ejercicio 1/TP6/Programa1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int final = 0;
+            Console.WriteLine("El numero es: " + numero);
+            while(numero >= 0)
+            {   
+                if(numero>=10)
+                {
+                    int resto = numero % 10;
+                    numero /= 10;
+                    final = resto + final*10;
+                }
+                else{
+                    final = (final*10) + numero;
+                    numero = 0;
+                    break;
+                }
+                
+            }
+            Console.WriteLine("El numero invertido es:" + final);
+        }
+    }
+}
---
+++ TP6/Ejercicio 2/Tp6-Punto2/Tp6-Punto2/Programa2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Numerics;
+
+namespace Tp6_Punto2
+{
+    class Programa2
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int valor = 1;
+            int resultado = 0;
+            
+            while(valor > 0)
+            {
+                Console.WriteLine("-----Elija la operación-----");
+                Console.WriteLine("1.Suma");
+                Console.WriteLine("2.Resta");
+                Console.WriteLine("3.Multiplicacion");
+                Console.WriteLine("4.Division");
+                valor = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el primer numero: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el segundo numero distinto de 0: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                if (num2 != 0)
+                {
+                    if(valor == 1)
+                    {
+                        resultado = num1 + num2;
+                    }else if(valor == 2)
+                    {
+                        if (num1 > num2)
+                        {
+                            resultado = num1 - num2;
+                        }
+                        else
+                        {
+                            resultado = num2 - num1;
+                        }
+                    }
+                    else if(valor == 3)
+                    {
+                        resultado = num1 * num2;
+                    }
+                    else
+                    {
+                        resultado = num1 / num2;
+                    }
+                    Console.WriteLine("El resultado de la operacion es: " + resultado);
+                    Console.WriteLine("Desea realizar otra operacion?");
+                    Console.Write("Para salir ingrese 0, para elegir otra operacion ingrese 1.");
+                    valor = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("El segundo numero no es adecuado");
+                }
+            }
+        }
+    }
+}
