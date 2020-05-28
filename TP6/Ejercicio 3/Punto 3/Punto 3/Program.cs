a3cabcdbf0200f482ec3b1e594461fbc5bcf2e27
Chicabytes <chocobarjulietain@gmail.com>
2020-05-27 21:01:22 -0300

    Se agrega el Ejercicio 3

---
+++ TP6/Ejercicio 3/Punto 3/Punto 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Punto_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Operaciones-----");
+            Console.WriteLine("Ingrese un numero: ");
+            float num1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Valor Absoluto: " + Math.Abs(num1));
+            Console.WriteLine("Su cuadrado: " + Math.Pow(num1, 2));
+            Console.WriteLine("Su raiz cuadrada: " + Math.Sqrt(num1));
+            Console.WriteLine("Seno: " + Math.Sin(num1));
+            Console.WriteLine("Coseno: " + Math.Cos(num1));
+            Console.WriteLine("La parte entera: " + Math.Round(num1));
+            Console.WriteLine("-----Maximo y Minimo-----");
+            Console.WriteLine("Ingrese el primer numero : ");
+            float num2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero : ");
+            float num3 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("El maximo entre los dos numeros: " + Math.Max(num2, num3));
+            Console.WriteLine("El minimo entre los dos numeros: " + Math.Min(num2, num3));
+        }
+    }
+}
